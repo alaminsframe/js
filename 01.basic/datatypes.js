d2282bd186efc66776d331ae5d908d7ffe371690
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-08-08 07:25:55 +0000

    datatypes

--- 01.basic/datatypes.js
+++ 01.basic/datatypes.js
@@ -4,3 +4,10 @@
 
 // reference (non primitive)
 // 3 types: array, object, function
+
+//console.log(typeof Symbol('2233'))
+
+let num1 = 2342412342134123412341234234523623565235532
+let num2 = 2342412n
+console.log(typeof num1)
+console.log(typeof num2)
