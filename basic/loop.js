d3faae58b65bf26259a731e7aae80b1c64429e08
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-09-14 17:54:55 +0000

    while and do while loop

--- basic/loop.js
+++ basic/loop.js
@@ -1,3 +1,54 @@
-for(i=1; i<=10; i++){
-    console.log(i*10)
-}
+// for(i=1; i<=10; i++){
+//     console.log(i*10)
+// }
+
+
+// let hello = [1,2,3,4,5,6,7]
+
+// for(i=0; i<hello.length; i++){
+//     if(i==3){
+//         break;
+//     }
+//     console.log(hello[i])
+// }
+
+
+
+
+// let hello = [1,2,3,4,5,6,7]
+
+// for(i=0; i<hello.length; i++){
+//     if(i==3){
+//         continue;
+//     }
+//     console.log(hello[i])
+// }
+
+
+
+
+// let x =1;
+
+// while(x<=10){
+//     console.log(x)
+//     x++
+// }
+
+
+// let hello = [1,2,3,4,5,6]
+
+// let x = 0;
+// while(x<hello.length){
+//     console.log(hello[x])
+//     x++
+// }
+
+
+
+
+// let x =1;
+
+// do{
+//     console.log(x)
+//     x+=2
+// }while(x<=20)
