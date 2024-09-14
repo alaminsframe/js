ecaf73ee6c672bbfe0550c9880fddfea5c34ff62
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-09-14 11:54:08 +0000

    array map

--- 01.basic/array.js
+++ 01.basic/array.js
@@ -1,14 +1,19 @@
+// const hello = [1,2,3,4,5,6,7];
+// //console.log(hello);
+// console.log(hello.filter( (item) => item <5));
+
+// const newHello =[];
+
+// hello.forEach( (item)=> {
+//     if(item > 3){
+//         newHello.push(item);
+//     }
+// });
+// console.log(newHello)
+// console.log(hello)
+
+
 const hello = [1,2,3,4,5,6,7];
-//console.log(hello);
-console.log(hello.filter( (item) => item >= 3));
+const newhello = hello.map( (item) => item *10).map( (item)=> item +5).filter((item)=> item>30);
 
-const newHello =[];
-
-hello.forEach( (item)=> {
-    if(item > 3){
-        newHello.push(item);
-    }
-});
-console.log(newHello)
-console.log(hello)
-
+console.log(newhello)
