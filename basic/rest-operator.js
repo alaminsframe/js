fbaaa6b7f3ae7dbb02cddc982264be9ca0bcc748
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-05 10:21:02 +0000

    m

--- basic/rest-operator.js
+++ basic/rest-operator.js
@@ -27,14 +27,14 @@
 
 
 
-function sum(str, ...nums){
+function sum(fname,lname, ...nums){
 
     sum = 0;
     for(i in nums){
         sum += nums[i];
     }
-    console.log(str)
+    console.log(`${fname} ${lname}`)
     console.log(sum)
 }
 
-sum('hello',1,32,4,6,7)
+sum('hello','hi',1,32,4,6,7,44,10,26)
