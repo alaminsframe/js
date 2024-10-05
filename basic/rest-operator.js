4c46091cd6d59bd24311ccfc764f70735016c0f9
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-05 10:54:41 +0000

    Spread Operator

--- basic/rest-operator.js
+++ basic/rest-operator.js
@@ -27,14 +27,52 @@
 
 
 
-function sum(fname,lname, ...nums){
+// function sum(fname,lname, ...nums){
 
-    sum = 0;
-    for(i in nums){
-        sum += nums[i];
-    }
-    console.log(`${fname} ${lname}`)
-    console.log(sum)
+//     sum = 0;
+//     for(i in nums){
+//         sum += nums[i];
+//     }
+//     console.log(`${fname} ${lname}`)
+//     console.log(sum)
+// }
+
+// sum('hello','hi',1,32,4,6,7,44,10,26)
+
+
+
+//##############    Spread Operator     #############
+
+
+// let arr1= [3,4,6,2,6,9]
+// let arr2= [30,40,60,20,60,90]
+// console.log([...arr1,...arr2])
+
+
+
+// let arr1= [3,4,6,2,6,9]
+// let arr2= [30,40,60,20,60,90]
+// console.log([1,2,...arr1,...arr2,100,200])
+
+
+let obj1 = {
+    fname:'hello',
+    lname:'hi'
 }
+let obj2 = {
+    age:20
+}
+console.log({...obj1,...obj2})
 
-sum('hello','hi',1,32,4,6,7,44,10,26)
+
+// function sum(fname,lname, ...nums){
+
+//     sum = 0;
+//     for(i in nums){
+//         sum += nums[i];
+//     }
+//     console.log(`${fname} ${lname}`)
+//     console.log(sum)
+// }
+//  let a = ['hello','hi',1,32,4,6,7,44,10,26]
+// sum(...a)
