2f7aaf3aa84a33d80ec7bb1d89693dfd30fa72ef
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-10 07:56:02 +0000

    Object literals

---
+++ basic/object-literal.js
@@ -0,0 +1,25 @@
+
+
+//##############  Normal  #############
+
+// let fname = 'hello ';
+// let lname = 'hi';
+
+// let obj = {
+//     firstName: fname,
+//     lastName: lname,
+// }
+// console.log(obj)
+
+
+
+//##############  obj literals  #############
+
+let fname = 'hello ';
+let lname = 'hi';
+
+let obj = {
+    firstName: fname,
+    lastName: lname,
+}
+console.log(obj)
