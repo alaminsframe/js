ca98a936bc5609a2e2f6f981bc3f6df825797308
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-10 19:17:42 +0000

    Destructuring Array

--- basic/destructuring.js
+++ basic/destructuring.js
@@ -1,6 +1,15 @@
-let arr = ['hello', 20]
+// let arr = ['hello', 20]
 
-let [name, age] = arr;
+// let [name, age] = arr;
 
-console.log(name);
-console.log(age)
+// console.log(name);
+// console.log(age)
+
+
+
+// let arr = ['hello',25 , 'dhaka']
+// let [fullName, age, city]= arr;
+
+// console.log(fullName)
+// console.log(age)
+// console.log(city)
