649659767b271c6e33b4307a0f20b36b13918136
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-10 08:25:45 +0000

    bj literals: new function syntax and obj or arr return function

--- basic/object-literal.js
+++ basic/object-literal.js
@@ -41,13 +41,64 @@
 
 
 
-let fname = 'First';
-let lname = 'LastName';
+// let fname = 'First';
+// let lname = 'LastName';
 
-let obj = {
-    [fname+'Name']: 'hello',
-    [lname]: 'hi',
-    age: 20
+// let obj = {
+//     [fname+'Name']: 'hello',
+//     [lname]: 'hi',
+//     age: 20
+// }
+// console.log(obj)
+// console.log(obj.FirstName)
+
+
+
+//##############  obj literals: new function syntax  #############
+
+////////// normal 
+// let fname = 'Hello';
+// let lname = 'Hi';
+
+// let obj = {
+//     fname,
+//     lname,
+//     fullName: function (){
+//         return `${this.fname} ${this.lname}`;
+//     },
+//     age: 20
+// }
+// console.log(obj.fullName())
+
+
+// let fname = 'Hello';
+// let lname = 'Hi';
+
+// let obj = {
+//     fname,
+//     lname,
+//     fullName: function (){
+//         return `${this.fname} ${this.lname}`;
+//     },
+//     age: 20
+// }
+//console.log(obj.fullName())
+
+
+
+// let fn = 'hello'
+// let ln = 'hi'
+
+// function ad(fn, ln){
+//     return {fn, ln}
+// }
+// console.log( ad(fn, ln))
+
+
+let fn = 'hello'
+let ln = 'hi'
+
+function ad(fn, ln){
+    return [fn+'hehe', ln]
 }
-console.log(obj)
-console.log(obj.FirstName)
+console.log( ad(fn, ln))
