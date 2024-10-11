7eb0841c0bb6239593e7e8fdbaf62aa89b7a7fd0
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-11 19:37:00 +0000

    constructor

--- advance/oop.js
+++ advance/oop.js
@@ -1,11 +1,27 @@
 //##### basic #####
 
-class hello{
-    message(){
-        console.log('hello world')
+// class hello{
+//     message(){
+//         console.log('hello world')
+//     }
+// }
+
+// let msg = new hello();
+// msg.message()
+// console.log(typeof msg)
+
+
+//##### constructor #####
+
+class student{
+    constructor(){
+        console.log('this is constructor function')
+    }
+
+    hello(){
+        console.log('this is normal function')
     }
 }
 
-let msg = new hello();
-msg.message()
-console.log(typeof msg)
+let std = new student(); //constructor function auto load first
+
