c07b43710b88ae7ae56b0013a6cedb7e1832ddf3
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-12 18:04:17 +0000

    constructor on inheritance

--- advance/inheritance.js
+++ advance/inheritance.js
@@ -1,11 +1,27 @@
+// class students{
+//     constructor(){
+//         console.log('this is constructor method')
+//     }
+
+// }
+
+// class goodStd extends students{
+
+// }
+// let obj = new goodStd();
+
+
 class students{
     constructor(){
-        console.log('this is constructor method')
+        console.log('this is constructor method of students')
     }
 
 }
 
 class goodStd extends students{
-
+    constructor(){
+        super();
+        console.log('this is constructor method of goodStd')
+    }
 }
 let obj = new goodStd();
