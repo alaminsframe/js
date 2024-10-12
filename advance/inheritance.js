fba7840fbff2f3eb8cdd54dbaebbb95c67dfb2fd
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-12 19:02:53 +0000

    use of inheritance

--- advance/inheritance.js
+++ advance/inheritance.js
@@ -51,23 +51,57 @@
 
 
 
-class students{
-    constructor(name){
-        this.name = name;
-        console.log('this is constructor method of students')
-    }
-    info(){
-        console.log(`student name is ${this.name}`)
-    }
+// class students{
+//     constructor(name){
+//         this.name = name;
+//         console.log('this is constructor method of students')
+//     }
+//     info(){
+//         console.log(`student name is ${this.name}`)
+//     }
 
-}
+// }
 
-class goodStd extends students{
-    info(){
-        super.info()
-        console.log(`Good student name is ${this.name}`)
-    }
+// class goodStd extends students{
+//     info(){
+//         super.info()
+//         console.log(`Good student name is ${this.name}`)
+//     }
        
+// }
+// let obj = new goodStd('hello hi');
+// obj.info()
+
+
+
+class employee{
+    constructor(name, age, salary){
+        this.empname = name;
+        this.empage = age;
+        this.empsalary= salary;
+        //console.log('constructor: employee')
+    }
+    info(){
+        console.log(`
+        All employee:
+        Name: ${this.empname}
+        Age: ${this.empage}
+        Salary: ${this.empsalary}`)
+    }
 }
-let obj = new goodStd('hello hi');
-obj.info()
+class manager extends employee{
+    info(){
+        let extra = 50;
+        let total = extra + this.empsalary;
+        console.log(`
+            For Manager:
+            Name: ${this.empname}
+            Age: ${this.empage}
+            Salary: ${total}`)
+    }
+}
+
+let obj1 = new manager('Rocky', 20, 100);
+let obj2 = new employee('Hocky', 22, 80);
+obj1.info()
+obj2.info()
