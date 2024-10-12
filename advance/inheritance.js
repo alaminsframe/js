5bf31a654e5f9cfb12875ba7fd52f285010beb49
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-12 18:34:48 +0000

    inheritance and super

--- advance/inheritance.js
+++ advance/inheritance.js
@@ -47,3 +47,27 @@
 // }
 // let obj = new goodStd('hello hi');
 // obj.info()
+
+
+
+
+class students{
+    constructor(name){
+        this.name = name;
+        console.log('this is constructor method of students')
+    }
+    info(){
+        console.log(`student name is ${this.name}`)
+    }
+
+}
+
+class goodStd extends students{
+    info(){
+        super.info()
+        console.log(`Good student name is ${this.name}`)
+    }
+       
+}
+let obj = new goodStd('hello hi');
+obj.info()
