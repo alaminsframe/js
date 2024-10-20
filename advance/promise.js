8e19e66ff67d89a7f70db4f85e74d7b459011ba9
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-20 16:29:15 +0000

    promise basic

--- advance/promise.js
+++ advance/promise.js
@@ -1,11 +1,35 @@
+const promisezero = new Promise(function(resolve, reject){
+    setTimeout(function(){
+      // console.log('task is complete');
+       resolve();
+       }, 1000)
+   }
+);
+
+promisezero.then(function(){
+   //console.log("promise comsumed");
+})
+
 const promiseOne = new Promise(function(resolve, reject){
      setTimeout(function(){
-        console.log('task is complete');
+       // console.log('task is complete');
         resolve();
         }, 1000)
     }
 );
 
 promiseOne.then(function(){
-    console.log("promise comsumed");
+    //console.log("promise comsumed");
+})
+
+
+promiseTwo = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Task two complete");
+        resolve()
+    },1000)
+})
+
+promiseTwo.then(function(){
+    console.log("resolved");
 })
