7c815c48908c4aae13408d6777e736f2dd58deb7
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-22 17:02:13 +0000

    promise all

--- advance/promise-all.js
+++ advance/promise-all.js
@@ -1,3 +1,29 @@
+// let p1 = new Promise( (resolve,reject)=>{
+//     setTimeout( ()=>{
+//         console.log("this is first promise");
+//         resolve(1);
+//     },1000)
+// })
+
+// let p2 = new Promise( (resolve,reject)=>{
+//     setTimeout( ()=>{
+//         console.log("this is second promise");
+//         resolve(2);
+//     },2000)
+// })
+
+// let p3 = new Promise( (resolve,reject)=>{
+//     setTimeout( ()=>{
+//         console.log("this is third promise");
+//         resolve(3);
+//     },3000)
+// })
+
+// Promise.all([p1,p2,p3]).then( (data)=>{console.log(data)}).catch( (data)=>{console.log(data)})
+
+
+
+
 let p1 = new Promise( (resolve,reject)=>{
     setTimeout( ()=>{
         console.log("this is first promise");
@@ -19,4 +45,19 @@ let p3 = new Promise( (resolve,reject)=>{
     },3000)
 })
 
-Promise.all([p1,p2,p3]).then( (data)=>{console.log(data)}).catch( (data)=>{console.log(data)})
+var  total = 0;
+Promise.all([p1,p2,p3])
+.then( (data)=>{
+    for(i in data){
+        total =+ data[i];
+    };
+    console.log(data) 
+    console.log(total);
+    
+})
+.catch( (data)=>{
+    console.log(data)
+})
+
+
+
