31dad66cc0e09aaf09f94525ae030d11947bbb49
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-22 15:24:23 +0000

    promise

--- advance/promise.js
+++ advance/promise.js
@@ -23,13 +23,38 @@ promiseOne.then(function(){
 })
 
 
-promiseTwo = new Promise(function(resolve, reject){
+const promiseTwo = new Promise(function(resolve, reject){
     setTimeout(function(){
-        console.log("Task two complete");
-        resolve()
+        resolve({name: "hello", mail: "hello@mail.com"})
     },1000)
 })
 
-promiseTwo.then(function(){
-    console.log("resolved");
+promiseTwo.then(function(userData){
+    //console.log(userData);
+})
+
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let err = false;
+        if(!err){
+            resolve({name: "hello", Pass: "123"})
+        }else{
+            reject("Error");
+        }
+    },1000)
+})
+
+const userName = promiseThree
+.then((userData)=>{
+    console.log(userData);
+    return userData.name;
+})
+.then((name)=> {console.log(name);
+})
+.catch((err)=>{console.log(err);
+})
+.finally(()=>{
+    console.log("this is final result");
+    
 })
