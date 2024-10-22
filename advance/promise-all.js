895f71dc99e49c88c9b80f33fb7dbffc0a214903
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-22 16:51:54 +0000

    promise all

---
+++ advance/promise-all.js
@@ -0,0 +1,22 @@
+let p1 = new Promise( (resolve,reject)=>{
+    setTimeout( ()=>{
+        console.log("this is first promise");
+        resolve(1);
+    },1000)
+})
+
+let p2 = new Promise( (resolve,reject)=>{
+    setTimeout( ()=>{
+        console.log("this is second promise");
+        resolve(2);
+    },2000)
+})
+
+let p3 = new Promise( (resolve,reject)=>{
+    setTimeout( ()=>{
+        console.log("this is third promise");
+        resolve(3);
+    },3000)
+})
+
+Promise.all([p1,p2,p3]).then( (data)=>{console.log(data)}).catch( (data)=>{console.log(data)})
