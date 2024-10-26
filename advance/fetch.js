53f70a3c97912779027a1e58f2e4e859cca4671e
MD. AL-AMIN SHEIKH <123836724+alaminsframe@users.noreply.github.com>
2024-10-26 18:17:43 +0000

    async await

--- advance/fetch.js
+++ advance/fetch.js
@@ -1,11 +1,20 @@
-function randomUser(){
-    fetch(`https://randomuser.me/api`)
-    .then( (raw) => {
-        return raw.json();
-    })
-    .then( (data) => {
-        console.log(data);
+// function randomUser(){
+//     fetch(`https://randomuser.me/api`)
+//     .then( (raw) => {
+//         return raw.json();
+//     })
+//     .then( (data) => {
+//         console.log(data);
         
-    })
+//     })
+// }
+// randomUser()
+
+
+
+async function randomUser() {
+    let apiUrl = await fetch(`https://randomuser.me/api`);
+    let rawData = await apiUrl.json();
+    console.log(rawData);
 }
-randomUser()
+randomUser();
